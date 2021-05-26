39efc32da7a2394fd969f465fdf491472c04d1d0
IgorSenychCW <igor.senych@cloudwalk.io>
2021-05-26 14:00:24 +0300

    Hardhat integrations.

---
+++ hardhat.config.ts
@@ -0,0 +1,31 @@
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.address);
+  }
+});
+
+export default {
+  solidity: {
+    version: "0.6.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    hardhat: {},
+  },
+};
